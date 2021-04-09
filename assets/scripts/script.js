93724243ec96a9459be71cf1eb870ac96138422f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-09 00:20:02 -0500

    lax.js scroll animations

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,3 +8,45 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
   });
 });
+
+window.onload = function () {
+  lax.init()
+
+  // Add a driver that we use to control our animations
+  lax.addDriver('scrollY', function () {
+    console.log(window.scrollY);
+    return window.scrollY
+  })
+
+  lax.addElements('.fade-in-out-effect', {
+    scrollY: {
+      opacity: [
+        ['elInY', 'elCenterY', 'elOutY'],
+        [0, 1, 0],
+      ],
+      translateY: [
+        ['elInY', 'elCenterY', 'elOutY'],
+        {
+          500: [0, 0, 0], // Screen width < 500
+          900: [-150, 0, 150], // Screen width > 500 and < 900
+        },
+      ]
+    }
+  })
+
+  lax.addElements('.fade-out-effect', {
+    scrollY: {
+      opacity: [
+        ['elInY', 'elCenterY', 'elOutY'],
+        [1, 1, 0],
+      ],
+      translateY: [
+        ['elInY', 'elCenterY', 'elOutY'],
+        {
+          500: [0, 0, 100], // Screen width < 500
+          900: [0, 0, 200], // Screen width > 500 and < 900
+        },
+      ]
+    }
+  })
+}
