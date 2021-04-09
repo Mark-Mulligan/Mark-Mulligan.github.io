f3fd9c3009e9a862115b629654bcf1c095e15654
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-04-09 00:31:39 -0500

    removed extra text

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,7 +10,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 window.onload = function () {
   lax.init()
 
-  // Add a driver that we use to control our animations
   lax.addDriver('scrollY', function () {
     return window.scrollY
   })
